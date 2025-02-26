dea23368d0e6b30c12ea7272ee871a0cdbf6bed2
“Rafly <cyberschoolindo@gmail.com>
2025-02-26 10:50:54 +0700

    mengubah command menjadi /mulaigame

---
+++ commands/utility/mulaigame.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("mulaigame")
+		.setDescription("Mulai permainan suit kertas, gunting, batu"),
+	async execute(interaction, gameStatus) {
+		if (gameStatus.isPlaying) {
+			await interaction.reply(
+				"Permainan sudah dimulai. Ketik /selesai untuk mengakhiri permainan."
+			);
+		} else {
+			gameStatus.isPlaying = true;
+			gameStatus.wins = 0;
+			gameStatus.losses = 0;
+			gameStatus.draws = 0;
+			await interaction.reply(
+				"Permainan dimulai! Ketik /kertas, /gunting, atau /batu untuk memilih."
+			);
+		}
+	},
+};

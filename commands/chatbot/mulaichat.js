f42a6bfd01b359774b766f634e81d08a82ec6c84
“Rafly <cyberschoolindo@gmail.com>
2025-03-06 17:52:22 +0700

    menambahkan embeds pada commands /mulaichat dan /selesaichat

--- commands/chatbot/mulaichat.js
+++ commands/chatbot/mulaichat.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -6,6 +6,21 @@ module.exports = {
 		.setDescription("Mulai Chat dengan oshi :D"),
 	async execute(interaction) {
 		global.chatSession = true;
-		await interaction.reply("Sesi chat dimulai! Silakan tanyakan sesuatu.");
+
+		const helpEmbed = new EmbedBuilder()
+			.setColor("White")
+			.setTitle("Let's Chat 💬")
+			.setDescription("Selamat datang di sesi chat bersama Zee Bot!")
+			.setThumbnail("https://i.pinimg.com/1200x/a0/57/26/a057261e198d49a21f9b3c3ad9461598.jpg")
+			.addFields(
+				{ name: "\u200B", value: "🔒 **Rules** 🔒" },
+				{ name: "Model AI 🤖", value: "> ➡️ `Falcon-7B-Instruct`", inline: true },
+				{ name: "Language 🏳️", value: "> ➡️ `English`", inline: true },
+				{ name: "\u200B", value: "⚠️ **Information** ⚠️" },
+				{ name: "Memulai Sesi Chat!", value: "Silahkan tanyakan sesuatu. Gunakan bahasa yang mudah dimengerti agar mendapatkan respon yang diinginkan. Ketik `/selesaichat` untuk mengakhiri sesi chat!", }
+			)
+			.setTimestamp();
+
+		await interaction.reply({ embeds: [helpEmbed] });
 	},
 };
--- commands/chatbot/selesaichat.js
+++ commands/chatbot/selesaichat.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,6 +6,18 @@ module.exports = {
         .setDescription("Akhiri Chat dengan oshi :D"),
     async execute(interaction) {
         global.chatSession = false;
-        await interaction.reply("Sesi Chat berakhir!");
+
+        const helpEmbed = new EmbedBuilder()
+			.setColor("White")
+			.setTitle("End's Chat 💬")
+			.setDescription("Terimakasih sudah berinteraksi dengan Zee Bot!")
+			.setThumbnail("https://i.pinimg.com/1200x/a0/57/26/a057261e198d49a21f9b3c3ad9461598.jpg")
+			.addFields(
+				{ name: "\u200B", value: "⚠️ **Information** ⚠️" },
+				{ name: "Mengakhiri Sesi Chat!", value: "Terimakasih sudah bertanya, semoga respon yang didapat sesuai harapan. Ketik `/mulaichat` untuk memulai kembali sesi chat!" }
+			)
+			.setTimestamp();
+
+		await interaction.reply({ embeds: [helpEmbed] });
     },
 };
